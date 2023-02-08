dc09966f1969259a34f5a6d14679bc5f7916697b
rylderoliveira <rylder2014@gmail.com>
2023-02-08 15:08:42 -0300

    :sparkles: TODO

--- app/src/main/java/com/impacta/compose/MainActivity.kt
+++ app/src/main/java/com/impacta/compose/MainActivity.kt
@@ -3,12 +3,6 @@ package com.impacta.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Device
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
-import com.impacta.compose.preview.MyPreviews
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +13,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@MyPreviews
-@Composable
-fun MyText() {
-    Text(
-        text = "Hello World"
-    )
-}
+// TODO (1) Adicionar todos os packages
+// TODO (2) Criar nossa primeira função Composable
+// TODO (3) Adicionar um preview para essa função
+// TODO (4) Adicionar muito previews
+// TODO (5) Remover código boilerplate
+// TODO (6) Trabalhando com Text, criar nosso texto customizado
+/*
+    Propriedades
+    - text
+    - style
+    - font size
+    - font weight
+    - fontStyle
+    - color
+    - font family
+    - text decoration
+    - shadow
+    - color
+    - blur
+    - off set
+    - background
+    - text indent
+    - spannable
+    - letter spacing
+    - max lines
+    - text align
+    - line height
+ */
